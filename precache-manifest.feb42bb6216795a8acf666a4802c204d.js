44eed37a5b1750872ec220d5534800525ceb0225
barami62 <wodbs7758@naver.com>
2020-09-19 20:19:16 +0900

    Updates

---
+++ precache-manifest.feb42bb6216795a8acf666a4802c204d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41e85ee8a7c618a4ddb7b7d409ac0fc",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "120a46e2b01559b4fc58",
+    "url": "/nwitter/static/css/main.3ad7fadf.chunk.css"
+  },
+  {
+    "revision": "f74995dc78276b96bea0",
+    "url": "/nwitter/static/js/2.c57c24ac.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.c57c24ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120a46e2b01559b4fc58",
+    "url": "/nwitter/static/js/main.37920226.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
